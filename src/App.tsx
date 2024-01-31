64c09b5c6b7a62043b0df3c5a3c4d612fcf48b30
Andrew Caetano <andrewr_caetano@hotmail.com>
2024-01-31 19:51:10 -0300

    Prerando componentes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+import { Login, NotFound, Search, Album, Favorites, Profile, ProfileEdit } from './pages';
+
 function App() {
   return (
-    <p>Trybetunes</p>
+    <>
+      <p>Trybetunes</p>
+      <Routes>
+        <Route path="/" element={ <Login /> } />
+        <Route path="/search" element={ <Search /> } />
+        <Route path="/album/:id" element={ <Album /> } />
+        <Route path="/favorites" element={ <Favorites /> } />
+        <Route path="/profile/edit" element={ <ProfileEdit /> } />
+        <Route path="/profile" element={ <Profile /> } />
+        <Route path="/*" element={ <NotFound /> } />
+      </Routes>
+    </>
   );
 }
 
